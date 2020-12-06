a860f3bb448839261a00c952dbcdcbc492c82afc
Tristan Martinuson <tmartinuson@gmail.com>
2020-12-06 16:40:22 -0600

    Fixed exception handling

--- server/service/shelter_data.py
+++ server/service/shelter_data.py
@@ -2,12 +2,30 @@ from .base import Base
 from ..model.shelter import Shelter
 
 
+class ShelterAlreadyExistsError(Exception):
+    pass
+
+
+class AccountNotFoundError(Exception):
+    pass
+
+
 class ShelterData(Base):
 
     def create(self, name, address, num_beds, coordinate_x, coordinate_y):
+        shelter = self.session.query(Shelter).filter(
+            Shelter.name == name).first()
+        if shelter:
+            raise ShelterAlreadyExistsError()
+
         shelter = Shelter(name, num_beds, address, coordinate_x, coordinate_y)
         self.session.add(shelter)
 
     def get(self, name):
+        shelter = self.session.query(Shelter).filter(
+            Shelter.name == name).first()
+        if not shelter:
+            raise AccountNotFoundError()
+        
         shelter = self.session.query(Shelter).filter(Shelter.name == name).first()
         return shelter
